d00a1c8b6f37f170b868966d4aed3f49cddc2ffd
Richard van 't Hof <richardvanthof@pm.me>
2019-04-22 20:02:56 +0200

    Added positioning + updated Readme
    Added a function to make all the images appear at a random position. BUG: width and height is not generated yet. Also split up the JS-code base in multiple files to make everything more readable. Finally I updated the instructions in the readme with the new location of the data.

---
+++ src/js/data.js
@@ -0,0 +1,27 @@
+// Data objects. All content (for now) is beain loaded from this object. Maybe later on content can be loaded in via JSON.
+const data = {
+    item1: {
+        img: 'static/img/1.jpg',
+        title: 'hello',
+        description: 'Lorem ipsum dolor sit amet item1',
+        url: '#1'
+    },
+    item2: {
+        img: 'static/img/2.gif',
+        title: 'item2',
+        description: 'Lorem ipsum dolor sit amet item2',
+        url: '#2'
+    },
+    item3: {
+        img: 'static/img/3.jpg',
+        title: 'item3',
+        description: 'Lorem ipsum dolor sit amet item3',
+        url: '#3'
+    },
+    item4: {
+        img: 'static/img/4.jpg',
+        title: 'item3',
+        description: 'Lorem ipsum dolor sit amet item3',
+        url: '#3'
+    }
+}
---
+++ src/js/thumbnail.js
@@ -0,0 +1,50 @@
+//Defines The thumbnail
+class Thumbnail {
+    constructor(img, title, description, url){
+        this.img = img;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.x;
+        this.y;
+        this.w;
+        this.h;
+    };
+
+    // Create HTML-element
+    display(){
+        const parent = document.querySelector('.container');
+
+        this.element = document.createElement('a');
+        this.element.href = this.url;
+        this.element.className = 'content';
+        this.element.innerHTML = '<img src='+ this.img +' alt='+this.title+'/>';
+        parent.insertBefore(this.element, parent.lastChild);
+
+    };
+
+    // Create and add position to element somewhere on the CSS grid.
+    position(){
+        // Calculate width
+        let generateSize = (pos) => {
+            let size;
+            while(size > 3) {
+                size = Math.floor(Math.random()*10/1.5);
+            }
+            return pos + size;
+        }
+
+        // Determine position and size + initiate variables
+        this.x = Math.floor(Math.random()*12 );
+        this.y = Math.floor(Math.random()*6 );
+        this.w = generateSize(this.x);
+        this.h = generateSize(this.y);
+
+        // Add CSS for size and witdth to element
+        this.element.style.gridColumnStart = this.x;
+        this.element.style.gridRowStart = this.y;
+
+        this.element.style.gridColumnEnd = this.w;
+        this.element.style.gridRowEnd = this.h;
+    }
+};
